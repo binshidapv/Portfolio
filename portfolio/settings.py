a1f4fea75c779508d60d0d6bbfc83dff00344b6a
binshidapv <binshida.pv@gmail.com>
2023-03-19 22:48:53 +0400

    intial commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import django_heroku
 import os
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.conf.urls.static import static
 from django.contrib import staticfiles
@@ -131,3 +131,4 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
