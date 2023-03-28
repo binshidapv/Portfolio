1fef2aec54da3dbe203fce696c8963022d89ab82
binshidapv <binshida.pv@gmail.com>
2023-03-28 13:56:49 +0400

    changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -121,8 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media/'
+
 
 
 
