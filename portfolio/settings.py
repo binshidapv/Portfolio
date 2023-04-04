bce888c857d8322c71da8ffdd54ad27fbbddda91
binshidapv <binshida.pv@gmail.com>
2023-04-04 16:57:11 +0400

    changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import django_heroku
 import os
 from pathlib import Path
+
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-from django.conf.urls.static import static
-from django.contrib import staticfiles
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -117,18 +117,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 #https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
-
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
