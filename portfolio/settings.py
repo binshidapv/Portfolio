7c2d2020a00d347ff2f6eac0faf959309d243fb5
binshidapv <binshida.pv@gmail.com>
2023-08-13 22:40:03 +0400

    settings file modified

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,17 +77,14 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
 
 
 
