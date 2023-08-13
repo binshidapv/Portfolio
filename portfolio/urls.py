b9a9beddb90683d17be12d69c98c8e99ffc3204c
binshidapv <binshida.pv@gmail.com>
2023-08-13 23:54:04 +0400

    urls modified

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -13,20 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+# Add any other necessary imports here (standard library, third-party, local)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("Portfolio_app.urls")),
-    ]
-# Serve static files during development
+    path('', include("Portfolio_app.urls")),
+]
+
+# Serve static and media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# Serve media files during development
-if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
