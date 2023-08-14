25682eb18523986e01acb3882818a3c8a66b5f6e
binshidapv <binshida.pv@gmail.com>
2023-08-14 14:52:58 +0400

    staticfiles

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,12 +18,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import  staticfiles_urlpatterns
 # Add any other necessary imports here (standard library, third-party, local)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("Portfolio_app.urls")),
 ]
+urlpatterns += staticfiles_urlpatterns()
+
 
 # Serve static and media files during development
 if settings.DEBUG:
